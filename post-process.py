3c166d1f8208502b5d73d8821943ea451b8776af
Sandun Darshana <sandundarshana01@gmail.com>
2026-05-19 22:23:59 +0200

    update post processing script

--- post-process.py
+++ post-process.py
@@ -1,5 +1,5 @@
 """
-Post-process the output of the simulation to extract velocity, temperature, and pressure contours, as well as streamlines. This script assumes that the simulation output is stored in a .csv format with columns for coordinates (x, y, z),velocity components (u, v, w), temperature (T), and pressure (p). All the data fields are expected to have a linear indexing format (i + j * nx + k * nx * ny), which will be rearranged into 3D arrays for visualization.
+Post-process the output of the simulation to extract velocity, temperature, and pressure contours, as well as streamlines. This script assumes that the simulation output is stored in a .csv/.txt format with columns for coordinates (x, y, z),velocity components (u, v, w), temperature (T), and pressure (p). All the data fields are expected to have a linear indexing format (i + j * nx + k * nx * ny), which will be rearranged into 3D arrays for visualization.
 
 author: Sandun
 """
@@ -35,18 +35,80 @@ def load_simulation_data(filename):
 
     return nx, ny, nz, x_3d, y_3d, z_3d, u_3d, v_3d, w_3d, T_3d, p_3d
 
-# function to plot velocity contours at a given z-slice
-def plot_velocity_contours(x_3d, y_3d, u_3d, v_3d, z_slice):
-    plt.figure()
-    plt.contourf(x_3d[:, :, z_slice], y_3d[:, :, z_slice], u_3d[:, :, z_slice], levels=50, cmap='jet')
-    plt.colorbar(label='Velocity u')
-    plt.title(f'Velocity Contours at z={z_slice}')
-    plt.xlabel('x')
-    plt.ylabel('y')
-    plt.show()
+class Plotter:
+    def __init__(self, x_3d, y_3d, z_3d, u_3d, v_3d, w_3d, T_3d, p_3d):
+        self.x_3d = x_3d
+        self.y_3d = y_3d
+        self.z_3d = z_3d
+        self.u_3d = u_3d
+        self.v_3d = v_3d
+        self.w_3d = w_3d
+        self.T_3d = T_3d
+        self.p_3d = p_3d
+
+    # plot XY plane velocity contours at a given z-slice
+    def plot_velocity_contours(self, z_slice):
+        fig, axes = plt.subplots(1, 3, figsize=(15, 5), sharex=True, sharey=True)
+
+        c0 = axes[0].contourf(
+            self.x_3d[:, :, z_slice], self.y_3d[:, :, z_slice], self.u_3d[:, :, z_slice], levels=50, cmap='jet'
+        )
+        fig.colorbar(c0, ax=axes[0], label='Velocity u')
+
+        c1 = axes[1].contourf(
+            self.x_3d[:, :, z_slice], self.y_3d[:, :, z_slice], self.v_3d[:, :, z_slice], levels=50, cmap='jet'
+        )
+        fig.colorbar(c1, ax=axes[1], label='Velocity v')
+
+        c2 = axes[2].contourf(
+            self.x_3d[:, :, z_slice], self.y_3d[:, :, z_slice], self.w_3d[:, :, z_slice], levels=50, cmap='jet'
+        )
+        fig.colorbar(c2, ax=axes[2], label='Velocity w')
+
+        fig.suptitle(f'Velocity Contours at z-slice {z_slice}')
+        fig.supxlabel('x')
+        fig.supylabel('y')
+        fig.tight_layout()
+        plt.show()
+
+    # plot XY plane streamlines at a given z-slice
+    def plot_streamlines(self, z_slice):
+        plt.figure(figsize=(6, 5))
+        plt.streamplot(self.x_3d[:, 0, z_slice], self.y_3d[0, :, z_slice], self.u_3d[:, :, z_slice].T, self.v_3d[:, :, z_slice].T, color='k', density=1.5)
+        plt.xlabel('x')
+        plt.ylabel('y')
+        plt.title(f'Streamlines at z-slice {z_slice}')
+        plt.tight_layout()
+        plt.show()
+
+    # plot XY plane temperature contours at a given z-slice
+    def plot_temperature_contours(self, z_slice):
+        plt.figure(figsize=(6, 5))
+        plt.contourf(self.x_3d[:, :, z_slice], self.y_3d[:, :, z_slice], self.T_3d[:, :, z_slice], levels=50, cmap='inferno')
+        plt.colorbar(label='Temperature T')
+        plt.xlabel('x')
+        plt.ylabel('y')
+        plt.title(f'Temperature Contour at z-slice {z_slice}')
+        plt.tight_layout()
+        plt.show()
+
+    # plot XY plane pressure contours at a given z-slice
+    def plot_pressure_contours(self, z_slice):
+        plt.figure(figsize=(6, 5))
+        plt.contourf(self.x_3d[:, :, z_slice], self.y_3d[:, :, z_slice], self.p_3d[:, :, z_slice], levels=50, cmap='viridis')
+        plt.colorbar(label='Pressure p')
+        plt.xlabel('x')
+        plt.ylabel('y')
+        plt.title(f'Pressure Contour at z-slice {z_slice}')
+        plt.tight_layout()
+        plt.show()
 
 # example usage
 filename = 'lbm_results.csv' # replace with actual filename
 
 nx, ny, nz, x_3d, y_3d, z_3d, u_3d, v_3d, w_3d, T_3d, p_3d = load_simulation_data(filename)
-plot_velocity_contours(x_3d, y_3d, u_3d, v_3d, z_slice=nz//2) # plot velocity contours at the first z-slice
+plotter = Plotter(x_3d, y_3d, z_3d, u_3d, v_3d, w_3d, T_3d, p_3d)
+plotter.plot_velocity_contours(z_slice=nz//2) # plot velocity contours
+plotter.plot_streamlines(z_slice=nz//2) # plot streamlines
+plotter.plot_temperature_contours(z_slice=nz//2) # plot temperature contours
+plotter.plot_pressure_contours(z_slice=nz//2) # plot pressure contours
